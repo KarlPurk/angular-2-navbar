05e50ca20a406603d3b1ef733eb1662862270d53
Karl Purkhardt <karl.purkhardt@nucleuscentral.com>
2016-11-03 19:57:44 +0000

    feat: allow custom templates

--- navbar.component.ts
+++ navbar.component.ts
@@ -1,20 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ContentChild, ViewChild, ViewContainerRef, TemplateRef } from '@angular/core';
 import { NavbarService, NavbarLink } from './navbar.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private navbarService: NavbarService) {
-   }
-
   private navbarLinks: NavbarLink[];
+
+  @ContentChild(TemplateRef) customTemplate: TemplateRef<Object>;
+  @ViewChild(TemplateRef) defaultTemplate: TemplateRef<Object>;
+
+  constructor(private viewContainer: ViewContainerRef,
+              private navbarService: NavbarService) {
+   }
 
   ngOnInit() {
     this.navbarLinks = this.navbarService.getNavbarLinks();
+  }
+
+  ngDoCheck() {
+
+    if (this.viewContainer.length > 0) {
+      return;
+    }
+
+    if (!this.defaultTemplate && !this.customTemplate) {
+      return;
+    }
+
+    if (this.customTemplate) {
+      this.viewContainer.createEmbeddedView.call(this.viewContainer, this.customTemplate, {
+        items: this.navbarLinks
+      });
+    }
+    else {
+      this.viewContainer.createEmbeddedView.call(this.viewContainer, this.defaultTemplate, {
+        items: this.navbarLinks
+      });
+    }
   }
 
 }
