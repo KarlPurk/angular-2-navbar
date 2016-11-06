4766fa648fdbb41b5f6dcab9c32b316bcbb46093
Karl Purkhardt <karl.purkhardt@nucleuscentral.com>
2016-11-06 19:48:59 +0000

    feat: add nav item weight property for sorting

--- navbar.service.ts
+++ navbar.service.ts
@@ -3,6 +3,7 @@ import { Injectable, Injector } from '@angular/core';
 export interface NavbarItem {
   route: string;
   label: string;
+  weight?: number;
   canDisplay?: Function[];
 }
 
@@ -12,6 +13,12 @@ export type NavbarItems = NavbarItem[];
 export class NavbarService {
 
   constructor(private items: NavbarItems, private injector: Injector) {
+    this.items = this.items.map((item) => {
+      if (item.weight === undefined) {
+        item.weight = 0;
+      }
+      return item;
+    });
   }
 
   getNavbarItems() {
@@ -21,7 +28,18 @@ export class NavbarService {
       }
       return item.canDisplay.filter((guard) => this.injector.get(guard).canActivate(item)).length;
     };
-    return this.items.filter((item) => canDisplay(item));
+    return this.items
+      .filter((item) => canDisplay(item))
+      .sort((a: NavbarItem, b: NavbarItem) => {
+        if (a.weight === b.weight) {
+          return 0;
+        }
+        if (a.weight < b.weight) {
+          return -1;
+        }
+        return 1;
+      });
   }
 
 }
+
